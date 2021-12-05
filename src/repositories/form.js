bf149d9c9ffce970609f25f6cacccffca5d873f5
Netto <netto@cofema.com.br>
2021-12-05 18:44:38 -0300

    addUser

--- src/repositories/form.js
+++ src/repositories/form.js
@@ -1,9 +1,8 @@
 import { api } from 'boot/axios'
-//import { api } from '../boot/axios'
+
 export async function getForm(){
     let rs = await api.get('/usuario/create')
     //console.log(rs.data) 
-    //return rs.data.response
     return rs.data
 }
 
