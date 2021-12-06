f7fff22d145a71c2978c2632a24df8a507d5078d
fernandof1987 <fernandof1987@gmail.com>
2021-12-05 23:15:21 -0300

    change negative color, dar mode default

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,35 @@
   <router-view />
 </template>
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted } from 'vue';
+import { useQuasar } from 'quasar'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  setup() {
+    const $q = useQuasar()
+
+    
+    onMounted( () => {
+      $q.dark.set(true)
+      document.body.style.setProperty('--q-negative', '#FF334C')
+      /*
+      if($q.dark.isActive){
+          document.body.style.setProperty('--q-negative', '#FF334C')
+      }
+      */
+    })
+
+  }
 })
 </script>
 
 <style>
 .body--light {
   background: #e1e1e1
+}
+
+.negative--dark {
+  color: green
 }
 </style>
